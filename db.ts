23c34fba8076ec90de1df51d3e155113907b09ed
dkbodiwala <88011896+dkbodiwala@users.noreply.github.com>
2021-07-26 19:22:24 -0600

    Added CryptoVisual project

---
+++ db.ts
@@ -0,0 +1,24 @@
+export default 
+{
+    "dataAlgo": [{
+        "id": 1,
+        "name": "Caesar Cipher",
+        "description": "Caesar Cipher is a very well-known substitute cipher. It was invented by Juilius Caesar. This chipher shifts all the characters in the text in order to make it appear meaningless. Sender and receiver has to agree on how much shift they want to use. This is called key. As there are 26 alphabets in Engish language, the key in Caesar Cipher should be between 1 to 26.",
+        "image": "assets/img/Ceaser-Cipher.png"
+    }, {
+        "id": 2,
+        "name": "Rail Fence Cipher",
+        "description": "Rail Fence Cipher is a Symmetric Key Cryptography Algorithm. It was named after a specific type of fence. In this cipher, plain text characters are rearranged in a fixed manner. The plain text characters are written in zig-zag manner based on the key provided. Therefore, in Rail Fence Cipher, key can only contain numbers.",
+        "image": "assets/img/Rail-Fence-Cipher.png"
+    }, {
+        "id": 3,
+        "name": "Advanced Encryption Standard (AES)",
+        "description": "AES has been the most accepted symmetric cypher since past a few years. US National Institute of Standard and Technology (NIST) had decided to look for a different and up to dated substitute of DES, and designed AES in 1997 which has overcome the drawbacks of 3DES and brute force attacks that were present before. After examining the several versions of AES, NIST announced the final standard of AES as the block cipher Rijndael which was developed by two Belgian cryptographers. The size of the block and key can be of following bits size: 128, 192, 256.",
+        "image": "assets/img/Aes-Cipher.jpg"
+    }, {
+        "id": 4,
+        "name": "RC4",
+        "description": "RC4 is a Stream Cipher. It was invented by Ron Rivest in 1987. In RC4, RC stands for Ron's Code. It was kept secret until 1994, and then it was published in ciber-punks mailing list. RC4 cipher has key size from 40 to 2048 bits, and it can contain digits, alphabets, and special characters. RC4 is simplest to implement and it is very fast.",
+        "image": "assets/img/RC4.png"
+    }]
+}
